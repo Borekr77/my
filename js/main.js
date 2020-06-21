30f2e050583998b922d0bedccf24ce49bdbe9564
eemly <revon78@gmail.com>
2020-06-21 20:35:49 +0200

    weather

--- js/main.js
+++ js/main.js
@@ -1,5 +1,43 @@
+
+
+//document.querySelector("#days-of-week").addEventListener("change", weatherOfDay);
+
+function extras() {
+
+    //let price = 1000;
+    let sum = parseInt(document.querySelector('#sauce').value);
+
+    let checkboxes = document.getElementsByName("CB");
+    let bChecked = true;
+
+    console.log("sum = " + sum, bChecked);
+
+    for (let i = 0; i < checkboxes.length; i++) {
+
+        if (checkboxes[i].checked) {
+            sum += parseInt(checkboxes[i].value);
+            bChecked = false;
+        }
+    }
+
+    if (bChecked) {
+        document.getElementById("nothing").checked = true;
+    } else {
+        document.getElementById("nothing").checked = false;
+    }
+
+
+    price += sum;
+
+    console.log("sum = " + sum);
+    console.log("price = " + price);
+}
+
+
+// A megrendelés összege:
 function calcAmount() {
-    let price = 1200;
+
+
     let amountInput = document.querySelector("input[name=amount-input]");
     let showAmount = document.querySelector("span.show-amount");
 
@@ -14,3 +52,11 @@ function calcAmount() {
         showAmount.innerHTML = amount;
     }
 }
+
+
+
+
+
+
+
+
---
+++ js/widget.js
@@ -0,0 +1,26 @@
+
+//Időjárás
+
+document.querySelector("#days-of-week").addEventListener("change", weatherOfDay);
+
+function weatherOfDay() {
+    let weekDegree = [2, 18, -1, 22, 30, 40, -10];
+    let offers = ["forró csoki", "meleg tea", "finom süti", "fagyi", "jéghideg limonádé"];
+    let maxDegree = [0, 15, 20, 25, 100];
+
+    let weekSelect = document.getElementById("days-of-week");
+    let valueSelectedDay = parseInt(weekSelect.options[weekSelect.selectedIndex].value);
+    let showDegree = document.querySelector("span#today-degree");
+    let showOffer = document.querySelector("span#today-offer");
+
+    showDegree.innerHTML = weekDegree[valueSelectedDay];
+
+    for (let i = 0; i < maxDegree.length; i++) {
+
+        if (weekDegree[valueSelectedDay] < maxDegree[i]) {
+            showOffer.innerHTML = offers[i];
+            break;
+        }
+    }
+}
+
