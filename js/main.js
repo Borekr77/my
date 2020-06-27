85b66a1d08deacc647095463a1ef63210a8127d2
eemly <revon78@gmail.com>
2020-06-28 01:28:38 +0200

    order, weather, users update

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+let deliveryText = document.createElement("small");
+deliveryText.className = "form-text text-muted";
+deliveryText.innerHTML = "Az ár tartalmazza a házhoz szállítási díjat! (5000 Ft alatti rendelés esetén: +500 Ft)";
+
+let parent = document.querySelector("#amount-text");
+parent.appendChild(deliveryText);
+deliveryText.style.display = "none";
 
 
 document.querySelector("#order-form").addEventListener("change", calcAmount);
@@ -34,7 +41,8 @@ function calcAmount() {
     }
 
 
-    //mennyiség 
+    //mennyiség
+    let amount = amountNumber * summa;
     if (amountNumber > 10) {
 
         amountInput.value = "1";
@@ -47,12 +55,42 @@ function calcAmount() {
         alert("Min 1!");
         showAmount.innerHTML = summa;
 
-    } else {
-        let amount = amountNumber * summa;
+    } else if (amount < 5000) {
+        amount += 500;
         showAmount.innerHTML = amount;
+        deliveryText.style.display = "block";
+    } else {
+        showAmount.innerHTML = amount;
+        deliveryText.style.display = "none";
     }
 
+}
 
+// Alert üzenet eltüntetése
+let alertCloseButtons = document.querySelectorAll(".close[data-dismiss='alert']");
+let alertCloseEventHandlerFunction = function (ev) {
+    this.parentElement.style.display = "none";
+};
+for (let i = 0; i < alertCloseButtons.length; i++) {
+    alertCloseButtons[i].addEventListener("click", alertCloseEventHandlerFunction);
 
+};
 
+//Szószok-select elem feltöltése
+let sauces = [
+    "szósz nélkül",
+    "csípős",
+    "majonéz",
+    "ketchup",
+    "BBQ"
+];
+
+let sauceSelect = document.querySelector("#sauceSelect");
+let k = 0;
+while (k < sauces.length) {
+    let option = document.createElement("option");
+    option.value = k;
+    option.innerHTML = sauces[k];
+    sauceSelect.appendChild(option);
+    k++;
 }
--- js/widget.js
+++ js/widget.js
@@ -1,51 +1,96 @@
+//Alapadatok
+let weekDegree = [2, 18, -1, 22, 30, 40, -10];
+let offers = ["forró csoki", "meleg tea", "finom süti", "fagyi", "jéghideg limonádé"];
+let intervalDegree = [0, 15, 20, 25, 100];
 
-//Időjárás
 
-// document.querySelector("#days-of-week").addEventListener("change", weatherOfDay);
+//Konverzó Fahrenheitre függvénye
+let celsiusToFahrenheit = function (celsiusTemp) {
+    return celsiusTemp / 5 * 9 + 32;
+}
 
 
-/* function weatherOfDay() {
-    let weekDegree = [2, 18, -1, 22, 30, 40, -10];
-    let offers = ["forró csoki", "meleg tea", "finom süti", "fagyi", "jéghideg limonádé"];
-    let maxDegree = [0, 15, 20, 25, 100];
-
-    let weekSelect = document.getElementById("days-of-week");
-    let valueSelectedDay = parseInt(weekSelect.options[weekSelect.selectedIndex].value);
-    let showDegree = document.querySelector("span#today-degree");
-    let showOffer = document.querySelector("span#today-offer");
-
-    showDegree.innerHTML = weekDegree[valueSelectedDay];
-
-    for (let i = 0; i < maxDegree.length; i++) {
-
-        if (weekDegree[valueSelectedDay] < maxDegree[i]) {
-            showOffer.innerHTML = offers[i];
-            break;
+//Heti minimum-hőmérséklet függvénye
+function minTemp(weekDegree) {
+    let min = weekDegree.length != 0 ? weekDegree[0] : 0;
+    for (let i = 1; i < weekDegree.length; i++) {
+        if (weekDegree[i] < min) {
+            min = weekDegree[i];
         }
     }
+    return min;
 }
- */
 
-document.addEventListener("change", function (e) {
-    var elem = e.target;
+//Heti maximum-hőmérséklet függvénye
+function maxTemp(weekDegree) {
+    let max = weekDegree.length != 0 ? weekDegree[0] : 0;
+    for (let i = 1; i < weekDegree.length; i++) {
+        if (weekDegree[i] > max) {
+            max = weekDegree[i];
+        }
+    }
+    return max;
+}
+
+//Heti átlaghőmérséklet függvénye
+function avgTemp(weekDegree) {
+    let avg = 0;
+    for (let i = 0; i < weekDegree.length; i++) {
+        avg = (avg + weekDegree[i]);
+    }
+    return weekDegree.length != 0 ? Math.round(avg / weekDegree.length) : 0;
+}
+
+
+
+/* Select-change-re lefutó függvény
+    - kiválasztott nap hőmérséklete
+    - ahhoz tartozó ajánlat
+    - min-max-avg
+    - fahrenheitben is
+*/
+
+/* document.addEventListener("load", function (ev) { alert(42) }); */
+document.addEventListener("change", function (ev) {
+    var elem = ev.target;
+
     if (elem && elem.id == "days-of-week") {
-        let weekDegree = [2, 18, -1, 22, 30, 40, -10];
-        let offers = ["forró csoki", "meleg tea", "finom süti", "fagyi", "jéghideg limonádé"];
-        let maxDegree = [0, 15, 20, 25, 100];
-
         let weekSelect = document.getElementById("days-of-week");
-        let valueSelectedDay = parseInt(weekSelect.options[weekSelect.selectedIndex].value);
         let showDegree = document.querySelector("span#today-degree");
+        let showFahrenheit = document.querySelector("span#today-fahrenheit");
         let showOffer = document.querySelector("span#today-offer");
 
+        let valueSelectedDay = parseInt(weekSelect.options[weekSelect.selectedIndex].value);
         showDegree.innerHTML = weekDegree[valueSelectedDay];
+        showFahrenheit.innerHTML = celsiusToFahrenheit(weekDegree[valueSelectedDay]);
 
-        for (let i = 0; i < maxDegree.length; i++) {
+        for (let i = 0; i < intervalDegree.length; i++) {
 
-            if (weekDegree[valueSelectedDay] < maxDegree[i]) {
+            if (weekDegree[valueSelectedDay] < intervalDegree[i]) {
                 showOffer.innerHTML = offers[i];
                 break;
             }
         }
     }
+
+    let minDegree = document.querySelector("span#min-degree");
+    let minFahrenheit = document.querySelector("span#min-fahrenheit");
+    let minCelsiusTemp = minTemp(weekDegree)
+    minDegree.innerHTML = minCelsiusTemp;
+    minFahrenheit.innerHTML = celsiusToFahrenheit(minCelsiusTemp);
+
+
+    let maxDegree = document.querySelector("span#max-degree");
+    let maxFahrenheit = document.querySelector("span#max-fahrenheit");
+    let maxCelsiusTemp = maxTemp(weekDegree)
+    maxDegree.innerHTML = maxCelsiusTemp;
+    maxFahrenheit.innerHTML = celsiusToFahrenheit(maxCelsiusTemp);
+
+
+    let avgDegree = document.querySelector("span#avg-degree");
+    let avgFahrenheit = document.querySelector("span#avg-fahrenheit");
+    let avgCelsiusTemp = avgTemp(weekDegree)
+    avgDegree.innerHTML = avgCelsiusTemp;
+    avgFahrenheit.innerHTML = celsiusToFahrenheit(avgCelsiusTemp);
+
 });
